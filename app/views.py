60de3ffa34aeaecc6299d54a6b9368cd5da97258
Synel <70210528+Synell@users.noreply.github.com>
2023-10-09 14:26:51 +0200

    register view

--- app/views.py
+++ app/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "index.html")
+    return render(request, 'index.html')
 
 def login(request: HttpRequest) -> HttpResponse:
-    return render(request, "login.html")
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    
+    if request.method == 'POST':
+        print(f'connect: {request.POST}')
+        return HttpResponseRedirect('/login')
 
 def register(request: HttpRequest) -> HttpResponse:
-    return render(request, "register.html")
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    
+    if request.method == 'POST':
+        print(f'register: {request.POST}')
+        return HttpResponseRedirect('/register')
