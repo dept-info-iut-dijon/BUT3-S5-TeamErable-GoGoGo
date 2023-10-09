835998e2ff13ef304920cfb5257f9ba37d2cba11
Synel <70210528+Synell@users.noreply.github.com>
2023-10-09 15:19:48 +0200

    removed useless code comments

--- app/views.py
+++ app/views.py
@@ -1,10 +1,1 @@
-# from .views.index import index
-# from .views.login import login_
-# from .views.register import register
 from .views import *
-
-# import os
-
-# for file in os.listdir(os.path.dirname(os.path.abspath(__file__)) + '/views/'):
-#     if file.endswith(".py"):
-#         __import__("app.views." + file[:-3], locals(), globals())
