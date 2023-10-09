6e6a686e3e3e4386acf659107f435f6018488811
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-10-09 15:53:28 +0100

    Partie BDD

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from app.models import Personne
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
+class PersonneProfile(admin.StackedInline):
+
+    model = Personne 
+
+
+
+class UserAdmin(UserAdmin):
+
+    inlines = (PersonneProfile,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Personne(models.Model):
+    
+    compte = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    amis = models.ManyToManyField('self', blank=True)
+
+    def __self__(self):
+        return self.nom
