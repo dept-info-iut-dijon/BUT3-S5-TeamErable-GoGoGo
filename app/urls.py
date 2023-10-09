b6ebf3c4e680c50f52b511346bceebc7a529e916
Synel <70210528+Synell@users.noreply.github.com>
2023-10-09 15:19:48 +0200

    removed useless code comments

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("login", views.login_, name="login"),
-    path("register", views.register, name="register"),
+    path("", views.index, name = "index"),
+    path("login", views.login_, name = "login"),
+    path("register", views.register, name = "register"),
 ]
