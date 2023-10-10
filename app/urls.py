a9f77e6f5b3b9a63a143b94494ef1eaa521bbe72
JohnGdr <john.gaudry@iut-dijon.u-bourgogne.fr>
2023-10-10 10:08:00 +0200

    AddFriends / DelFriends View

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_, name="login"),
     path("register", views.register, name="register"),
-    path("friends", views.friends, name="friends")
+    path("friends", views.friends, name="friends"),
+    path("delfriends", views.delfriends, name="delfriends"),
+    path("addfriends", views.addfriends, name="addfriends")
 ]
