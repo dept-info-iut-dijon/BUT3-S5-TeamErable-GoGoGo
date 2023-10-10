ea602a11d3e8e0e7c193b178f08a08b67dc31094
Lynn-Mei <hugo.murura@gmail.com>
2023-10-10 10:45:26 +0200

    Utilisation d'une classe user custom

---
+++ app/models_dir/person.py
@@ -0,0 +1,23 @@
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+
+class CustomUser(AbstractUser):
+    picturepath = models.CharField(max_length=255)
+    friends = models.ManyToManyField('self', blank=True)
+     # Add related_name to avoid clashes
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        related_name='customuser_set',  # You can choose a related_name of your choice
+        related_query_name='customuser'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        related_name='customuser_set',  # You can choose a related_name of your choice
+        related_query_name='customuser'
+    )
+
