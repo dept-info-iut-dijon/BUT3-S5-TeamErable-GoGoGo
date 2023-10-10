bf36e38638f8d2e9f61bfebd0479fd37d2b5bf58
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-10-10 11:12:19 +0100

    création du dossier models

--- app/models.py
+++ app/models.py
@@ -1,13 +1,3 @@
-from django.db import models
-from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractUser
+from .models import *
 
 # Create your models here.
-class Personne(models.Model):
-    
-    compte = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    amis = models.ManyToManyField('self', blank=True)
-
-    def __self__(self):
-        return self.nom
