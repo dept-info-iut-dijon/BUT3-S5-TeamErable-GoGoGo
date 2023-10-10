ebcc1ced0618405fd64b521b9c9fe15bc70f7c25
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-10-10 14:03:49 +0100

    Modification de l'attribut pathimage en blob

--- app/models/person.py
+++ app/models/person.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class CustomUser(AbstractUser):
-    picturepath = models.CharField(max_length=255)
+    blob = models.ImageField(max_length=5000, null=True)
     friends = models.ManyToManyField('self', blank=True)
      # Add related_name to avoid clashes
     groups = models.ManyToManyField(
