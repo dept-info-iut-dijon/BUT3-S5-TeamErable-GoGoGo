43ae39b76a2170fd7b3fcb32db86ad95da8ac789
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-10-10 11:12:19 +0100

    création du dossier models

--- app/admin.py
+++ app/admin.py
@@ -1,19 +1,5 @@
 from django.contrib import admin
-from app.models import Personne
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 # Register your models here.
-class PersonneProfile(admin.StackedInline):
-
-    model = Personne 
-
-
-
-class UserAdmin(UserAdmin):
-
-    inlines = (PersonneProfile,)
-
-
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
