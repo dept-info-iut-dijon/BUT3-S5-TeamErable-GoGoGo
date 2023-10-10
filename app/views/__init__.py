1e86ca9f22e68fadad59ccf764586933bf1b12ad
JohnGdr <john.gaudry@iut-dijon.u-bourgogne.fr>
2023-10-10 10:08:00 +0200

    AddFriends / DelFriends View

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -2,3 +2,5 @@ from .index import index
 from .login import login_
 from .register import register
 from .friends import friends
+from .delfriends import delfriends
+from .addfriends import addfriends
