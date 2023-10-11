f6685a388663d1e958d3243a84ba43c60ba589e0
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-10-11 13:57:00 +0100

    push pour John

--- app/views/profil.py
+++ app/views/profil.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from ..forms.Profil import Profil
 
 def profil(request: HttpRequest) -> HttpResponse:
-    if not request.user.is_authenticated: return HttpResponseRedirect('/login')
-    return render(request, 'profil.html')
+    donnees = {
+        'username': "ffff",
+        'email' : "sdfnsifo@sfissdi.sfsijf",
+        'password1' : "sfdsf",
+        'password2' : "ondfsdnf",
+    }
+
+    form = Profil(request.POST, initial=donnees)
+
+    if not request.user.is_authenticated: 
+        return HttpResponseRedirect('/login')
+
+    #if request.method == 'GET':
+    
+    
+    print(form)
+    return render(request, 'profil.html', {'form':form})
