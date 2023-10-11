01d7c520988c4c356b840f3373794e6d512cbaa3
Synel <montagna.samuel@gmail.com>
2023-10-11 15:15:24 +0200

    profil little update

--- app/views/profil.py
+++ app/views/profil.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
-from ..forms.Profil import Profil
 
 def profil(request: HttpRequest) -> HttpResponse:
-    donnees = {
-        'username': "ffff",
-        'email' : "sdfnsifo@sfissdi.sfsijf",
-        'password1' : "sfdsf",
-        'password2' : "ondfsdnf",
-    }
-
-    form = Profil(request.POST, initial=donnees)
-
     if not request.user.is_authenticated: 
         return HttpResponseRedirect('/login')
 
-    #if request.method == 'GET':
-    
-    
-    print(form)
-    return render(request, 'profil.html', {'form':form})
+    if request.method == 'GET':
+        return render(request, 'profil.html', {'user': request.user})
