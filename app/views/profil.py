5e560296f195f7222cd7a3c88c1b166343d77e17
Lynn-Mei <hugo.murura@gmail.com>
2023-10-12 09:33:30 +0200

    Backend du formulaire de mise a jour profil

--- app/views/profil.py
+++ app/views/profil.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from ..forms.Profil import Profil
 
 def profil(request: HttpRequest) -> HttpResponse:
     if not request.user.is_authenticated: 
@@ -7,3 +8,16 @@ def profil(request: HttpRequest) -> HttpResponse:
 
     if request.method == 'GET':
         return render(request, 'profil.html', {'user': request.user})
+
+    if request.method == 'POST':
+        form = Profil(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            print(request.FILES)
+            if 'profile_picture' in request.FILES:
+                request.user.profile_picture.save(request.FILES['profile_picture'].name, request.FILES['profile_picture'])
+            form.save()
+            
+    else:
+        form = Profil(instance=request.user)
+
+    return render(request, 'profil.html', {'form': form})
