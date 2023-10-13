003552a8032576d680039973686b4915f094ae71
Synel <70210528+Synell@users.noreply.github.com>
2023-10-13 14:32:55 +0200

    removed self and friends from search user

--- static/js/profil.js
+++ static/js/profil.js
@@ -105,7 +105,7 @@ document.querySelector("#search-input").addEventListener('keyup', function(event
     var csrf_token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
 
     var request = new XMLHttpRequest();
-    request.open('POST', '/search-user');
+    request.open('POST', '/search-notfriend-user');
     request.setRequestHeader('X-CSRFToken', csrf_token);
     request.onload = function() {
         if (request.status == 200) {
