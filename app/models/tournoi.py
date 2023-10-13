a8ecda23fb26af9171b94d1fa868c37d55c42cc9
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-10-13 09:44:55 +0100

    Ajout de la Table tournoi et Participer

---
+++ app/models/tournoi.py
@@ -0,0 +1,18 @@
+from django.db import models
+from .person import Person
+
+class Tournoi(models.Model):
+    Nom = models.CharField(max_length=255)
+    DateInscription = models.DateField()
+    DateDebut = models.DateField()
+    DateFin = models.DateField()
+    Organisateur = models.CharField(max_length=255)
+    Code = models.IntegerField(max_length=25)
+    Gagnant = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Participer(models.Model):
+    personne = models.ForeignKey(Person, on_delete=models.CASCADE)
+    tournoi = models.ForeignKey(Tournoi, on_delete=models.CASCADE)
