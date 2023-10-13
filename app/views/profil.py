f5ed1b3f73b0a82d090966aaf6272fd3683fba17
Synel <70210528+Synell@users.noreply.github.com>
2023-10-13 09:39:16 +0200

    password modification in profile settings

--- app/views/profil.py
+++ app/views/profil.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, HttpResponseBadRequest
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.password_validation import validate_password
 
 def profil(request: HttpRequest) -> HttpResponse:
     if not request.user.is_authenticated: return HttpResponseRedirect('/login')
@@ -37,7 +39,7 @@ def change_pfp(request: HttpRequest) -> HttpResponse:
     return HttpResponse('<p class="error">Une erreur est survenue.</p>')
 
 def change_user_info(request: HttpRequest) -> HttpResponse:
-    if not request.user.is_authenticated: return HttpResponseRedirect('/login')
+    if not request.user.is_authenticated: return HttpResponseBadRequest()
 
     if request.method == 'POST':
         name = request.POST.get('username')
@@ -54,7 +56,7 @@ def change_user_info(request: HttpRequest) -> HttpResponse:
     return HttpResponse('<p class="error">Une erreur est survenue.</p>')
 
 def change_pfp(request: HttpRequest) -> HttpResponse:
-    if not request.user.is_authenticated: return HttpResponseRedirect('/login')
+    if not request.user.is_authenticated: return HttpResponseBadRequest()
 
     if request.method == 'POST':
         pfp = request.FILES.get('pfp')
@@ -64,3 +66,33 @@ def change_pfp(request: HttpRequest) -> HttpResponse:
             return HttpResponse('<p class="success">La photo de profil a bien été modifiée.</p>')
     
     return HttpResponse('<p class="error">Une erreur est survenue.</p>')
+
+def change_pwd(request: HttpRequest) -> HttpResponse:
+    if not request.user.is_authenticated: return HttpResponseBadRequest()
+
+    if request.method == 'POST':
+        old_pwd = request.POST.get('old-pwd')
+        new_pwd = request.POST.get('new-pwd')
+        confirm_new_pwd = request.POST.get('new-pwd-confirm')
+
+        if old_pwd and new_pwd and confirm_new_pwd:
+            if new_pwd == confirm_new_pwd:
+                try:
+                    validate_password(new_pwd)
+
+                    if request.user.check_password(old_pwd):
+                        request.user.set_password(new_pwd)
+                        request.user.save()
+                        request.user = authenticate(username=request.user.username, password=new_pwd)
+                        if request.user: login(request, request.user)
+                        return HttpResponse('<p class="success">Le mot de passe a bien été modifié. Rechargement de la page...</p>')
+
+                    else:
+                        return HttpResponseBadRequest('<p class="error">L\'ancien mot de passe est incorrect.</p>')
+
+                except Exception as e:
+                    return HttpResponseBadRequest('<p class="error">Le mot de passe n\'est pas assez sécurisé.</p>')
+
+            return HttpResponseBadRequest('<p class="error">Les mots de passe ne correspondent pas.</p>')
+
+    return HttpResponseBadRequest('<p class="error">Une erreur est survenue. Vérifiez que vous avez bien rempli tous les champs.</p>')
