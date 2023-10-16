d9bc2223e7f994cfb4c94d878ae0b03925edd49d
Synel <70210528+Synell@users.noreply.github.com>
2023-10-16 13:51:30 +0200

    fix french to english

--- app/views/aide.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
-
-def aide(request: HttpRequest) -> HttpResponse:
-    
-    
-    return render(request, 'aide.html')
