469553d1eb3f1a35af1bb40eff9c9e08a525de3f
JohnGdr <john.gaudry@iut-dijon.u-bourgogne.fr>
2023-10-16 13:47:32 +0200

    suppression des init et routes inutile

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -2,10 +2,7 @@ from .reusable import *
 from .index import index
 from .login import login_, logout_
 from .register import register
-from .friends import friends
 from .settings import settings
-from .delfriends import delfriends
-from .addfriends import addfriends
 from .profil import profil, change_pfp, change_user_info, change_pwd, search_notfriend_user, friend_list, add_friend, delete_friend
 from .termsofuse import termsofuse
 from .privacypolicy import privacypolicy
