a2af663e37dd2b353c1ef57b892787ad563de0dc
JohnGdr <john.gaudry@iut-dijon.u-bourgogne.fr>
2023-10-16 10:30:42 +0200

    Suppression des fichier plus utilisé

--- app/views/friends.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
-
-def friends(request: HttpRequest) -> HttpResponse:
-    
-    return render(request, 'friends.html')
