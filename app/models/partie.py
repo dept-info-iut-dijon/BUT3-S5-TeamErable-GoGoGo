943df5012918499b88faaf82cdf50414c92983b9
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-10-16 14:37:19 +0100

    BDD partie

---
+++ app/models/partie.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .person import CustomUser
+
+class Partie(models.Model):
+    idPartie = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    dateDebut = models.DateField()
+    duree = models.IntegerField()
+    termine = models.BooleanField(default=False)
+    joueur1 = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='joueur1')
+    joueur2 = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='joueur2')
