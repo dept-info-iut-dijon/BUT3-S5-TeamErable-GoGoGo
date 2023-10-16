0b82d093582262186e77b77ef38135d2bc06b94c
Synel <70210528+Synell@users.noreply.github.com>
2023-10-16 18:41:52 +0200

    render test

--- GoGoGo/settings.py
+++ GoGoGo/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uqjwerik@nj-j!gm1876da)6%m3ws1arm)u2v352p!^ggrm9h@'
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-uqjwerik@nj-j!gm1876da)6%m3ws1arm)u2v352p!^ggrm9h@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['*']
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 AUTH_USER_MODEL = 'app.CustomUser'
 
