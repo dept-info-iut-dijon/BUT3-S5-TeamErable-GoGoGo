e798b1d9f7f939d7b92ba7eceee2bad7c40243db
Synel <70210528+Synell@users.noreply.github.com>
2023-10-17 14:31:20 +0200

    fixed signup error not showing

--- app/views/register.py
+++ app/views/register.py
@@ -21,8 +21,6 @@ def register(request: HttpRequest) -> HttpResponse:
             return HttpResponseRedirect('/')
 
         else:
-            # Check if the "Password2 didn't match" error occurred
-            if 'password2' in form.errors:
-                password2_error = "Vos mots de passe ne correspondent pas ou ne sont pas corrects."
+            password2_error = form.errors
 
         return render(request, 'signup.html', {'form':form, 'password2_error':password2_error})
