56fe327d6d8d1f9c80efef4512753f099f18dd68
Synel <montagna.samuel@gmail.com>
2023-10-17 11:44:00 +0200

    fixed game db stuff

--- app/views/game.py
+++ app/views/game.py
@@ -6,8 +6,12 @@ def game(request: HttpRequest) -> HttpResponse:
     if not request.user.is_authenticated: return HttpResponseRedirect('/login')
     if (game_id := request.GET.get('id')) is None: return HttpResponseRedirect('/')
 
-    game_inst = Game.objects.get(id_game = game_id, code = None, done = False)
-    if not game_inst: return HttpResponseRedirect('/')
+    try:
+        game_inst = Game.objects.get(id_game = game_id, code = None, done = False)
+        if not game_inst: return HttpResponseRedirect('/')
+
+    except:
+        return HttpResponseRedirect('/')
 
     if game_inst.player1 != request.user and game_inst.player2 is None:
         game_inst.player2 = request.user
@@ -21,8 +25,12 @@ def game_code(request: HttpRequest) -> HttpResponse:
     if not request.user.is_authenticated: return HttpResponseRedirect('/login')
     if (game_code := request.GET.get('code')) is None: return HttpResponseRedirect('/')
 
-    game_inst = Game.objects.get(code = game_code, done = False)
-    if not game_inst: return HttpResponseRedirect('/')
+    try:
+        game_inst = Game.objects.get(code = game_code, done = False)
+        if not game_inst: return HttpResponseRedirect('/')
+
+    except:
+        return HttpResponseRedirect('/')
 
     if game_inst.player1 != request.user and game_inst.player2 is None:
         game_inst.player2 = request.user
