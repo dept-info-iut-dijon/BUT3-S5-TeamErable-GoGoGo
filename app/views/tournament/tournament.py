87ea16469344c85a39b7e89087c3ae629ad6d5ef
Lynn-Mei <hugo.murura@gmail.com>
2023-10-18 08:57:22 +0200

    Correction de bugs et des noms de chemins

--- app/views/tournament/tournament.py
+++ app/views/tournament/tournament.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 
-def new_tournois(request: HttpRequest) -> HttpResponse:
+def tournament(request: HttpRequest) -> HttpResponse:
     if not request.user.is_authenticated: return HttpResponseRedirect('/login')
     
-    return render(request, 'tournois/newtournois.html')
+    return render(request, 'tournament/tournament.html')
