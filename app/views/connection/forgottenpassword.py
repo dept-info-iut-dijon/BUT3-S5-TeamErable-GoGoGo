b48673bb42d4d4ac29a3bd12472e46aa119f676a
Hugo-Mrula <hugo.mrula@iut-dijon.u-bourgogne.fr>
2023-11-14 15:46:47 +0100

    Optimisation des view de connection

--- app/views/connection/forgottenpassword.py
+++ app/views/connection/forgottenpassword.py
@@ -22,14 +22,14 @@ def send_password_reset_email(user: CustomUser, token: bytes | str) -> None:
     subject = "Recuperation de votre mot de passe"
     message = f"<h2>Bonjour, {user.username}!</h2>"
     message += "<p>Vous avez fait une demande de recuperation de mot de passe.</p> <p>Pour reinitialiser votre mot de passe, cliquez sur le lien ci dessous:</p>"
-    # Generattion de l'url
+    # Generation de l'url
     reset_url = reverse('password_reset_confirm', kwargs={'uidb64': uidb64, 'token': token})
-    reset_url = "<a href='http://localhost:8080" + reset_url + "'>Cliquez ici pour reinitialiser votre mot de passe</a>"  # Adjust the protocol and domain as needed in production
+    reset_url = "<a href='http://localhost:8080" + reset_url + "'>Cliquez ici pour reinitialiser votre mot de passe</a>"  
 
     message += reset_url + "\n\n"
     message += "<p>Si vous n'etes pas a l'origine de cette demande, ignorez cet email.</p>"
     message += "<p>L'equipe Go-Go-Go</p>"
-    # Send the email
+    # Envoie du mail
     from_email = settings.DEFAULT_FROM_EMAIL  # Use the default from_email from settings
     recipient_list = [user.email]
     send_mail(subject, message,from_email, recipient_list, html_message=message, fail_silently=False)
@@ -46,27 +46,25 @@ def forgotten_password(request: HttpRequest) -> HttpResponse:
     Returns:
         HttpResponse: La reponse HTTP a la requete de recuperation de mot de passe
     '''
+    ret = HttpResponseBadRequest('<p class="error">L''email n''a pas pu etre envoye</p>')
+    if request.method == RequestType.GET.value:
+        form = ForgottenPassForm()
+        ret = render(request, 'connection/forgottenpassword.html', {'form':form})
     if request.method == RequestType.POST.value:
         form = ForgottenPassForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
             try:
-                print("try")
                 #On verifie si le un utilisateur a cette email
                 user = CustomUser.objects.get(email=email)
-                print("found")
                 # Geneneration d'un token
                 token = default_token_generator.make_token(user)
-                print("token")
                 # Envoie du mail 
                 send_password_reset_email(user, token)
                 # Redirect to a "Password Reset Requested" page
-                print("sent")
-                return render(request, 'password_reset_request_success.html')
+                ret = render(request, 'password_reset_request_success.html')
             except CustomUser.DoesNotExist:
                 # Handle the case where the email is not found
-                pass
-    elif request.method == RequestType.GET.value:
-        form = ForgottenPassForm()
+                ret = HttpResponseBadRequest('<p class="error">Utilisateur introuvable</p>')
 
-    return render(request, 'connection/forgottenpassword.html', {'form':form})
+    return ret 
