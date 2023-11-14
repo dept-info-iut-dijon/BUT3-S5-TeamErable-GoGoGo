2a664bdf1ec7962873ed9668fad83a369db0e4d1
Synel <70210528+Synell@users.noreply.github.com>
2023-11-14 09:50:54 +0100

    correction docs et return sur forgottenpassword, passwordresetconfirm

--- app/views/passwordresetconfirm.py
+++ app/views/passwordresetconfirm.py
@@ -1,19 +1,34 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth import get_user_model
 from django.utils.http import urlsafe_base64_decode
-from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.http import HttpResponse, HttpRequest, HttpResponseBadRequest
 from django.utils.http import urlsafe_base64_decode
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from ..forms.ResetPasswordForm import ResetPasswordForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login
+from .decorators import logout_required, request_type, RequestType
 
-def password_reset_confirm(request: HttpRequest, uidb64, token) -> HttpResponse:
+@logout_required
+@request_type(RequestType.POST)
+def password_reset_confirm(request: HttpRequest, uidb64: bytes | str, token: bytes | str) -> HttpResponse:
+    '''Controlleur de la page de confirmation de réinitialisation de mot de passe
+
+    Args:
+        request (HttpRequest): La requête HTTP
+        uidb64 (_type_): ID de l'utilisateur
+        token (_type_): Token de réinitialisation de mot de passe
+
+    Returns:
+        HttpResponse: La réponse HTTP à la requête de confirmation de réinitialisation de mot de passe
+    '''
+    ret: HttpResponse = HttpResponseBadRequest()
+
     try:
         user_id = urlsafe_base64_decode(uidb64).decode()
         User = get_user_model()
-        user = User.objects.get(pk=user_id)
+        user = User.objects.get(pk = user_id)
 
-        if request.method == 'POST':
+        if request.method == RequestType.POST.value:
             form = ResetPasswordForm(request.POST)
             if form.is_valid():
                 # Assuming the form is valid, update the user's password
@@ -24,12 +39,14 @@ def password_reset_confirm(request: HttpRequest, uidb64, token) -> HttpResponse:
                 # Log the user in (optional)
                 login(request, user)
 
-                return render(request, 'profil.html')
+                ret = render(request, 'profil.html')
 
-        if default_token_generator.check_token(user, token):
-            return render(request, 'password_reset_form.html', {'form':ResetPasswordForm(),'user':user, 'token':token})
+        elif default_token_generator.check_token(user, token):
+            ret = render(request, 'password_reset_form.html', {'form':ResetPasswordForm(),'user':user, 'token':token})
         else:
-            return render(request, 'password_reset_token_invalid.html')
+            ret = render(request, 'password_reset_token_invalid.html')
 
     except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        return render(request, 'password_reset_token_invalid.html')
+        ret = render(request, 'password_reset_token_invalid.html')
+
+    return ret
