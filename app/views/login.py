d716f291fdd184273ce7499392607dc1ae096c16
Synel <70210528+Synell@users.noreply.github.com>
2023-11-14 09:05:32 +0100

    correction docs et return sur login, register

--- app/views/login.py
+++ app/views/login.py
@@ -2,29 +2,51 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, HttpResponseBadRequest
 from django.contrib.auth import authenticate, login, logout
 from ..forms.ConnectForm import ConnectForm
+from .decorators import login_required, logout_required, request_type, RequestType
 
 
-
+@logout_required
+@request_type(RequestType.GET, RequestType.POST)
 def login_(request: HttpRequest) -> HttpResponse:
-    if request.user.is_authenticated: return HttpResponseRedirect('/')
+    '''Controlleur de la page de connexion
 
-    if request.method == 'GET':
-        return render(request, 'login.html')
+    Args:
+        request (HttpRequest): La requête HTTP
+
+    Returns:
+        HttpResponse: La réponse HTTP à la requête de connexion
+    '''
+
+    ret: HttpResponse = HttpResponseBadRequest()
+
+    if request.method == RequestType.GET.value:
+        ret = render(request, 'login.html')
     
-    if request.method == 'POST':
+    elif request.method == RequestType.POST.value:
         form = ConnectForm(request.POST)
         if form.is_valid():
             user = authenticate(request, username = request.POST['username'], password = request.POST['password'])
             if user:
                 login(request, user)
-                return HttpResponseRedirect('/')
+                ret = HttpResponseRedirect('/')
 
-        return HttpResponseBadRequest('<p class="error">Le mot de passe ou le nom d\'utilisateur est incorrect.</p>')
+        else: ret = HttpResponseBadRequest('<p class="error">Le mot de passe ou le nom d\'utilisateur est incorrect.</p>')
         
-    return HttpResponseBadRequest()
+    return ret
 
 
 
+@login_required
+@request_type(RequestType.GET, RequestType.POST)
 def logout_(request: HttpRequest) -> HttpResponse:
+    '''Controlleur de la page de déconnexion
+
+    Args:
+        request (HttpRequest): La requête HTTP
+
+    Returns:
+        HttpResponse: La réponse HTTP à la requête de déconnexion
+    '''
+
     logout(request)
     return HttpResponseRedirect('/login')
--- app/views/register.py
+++ app/views/register.py
@@ -2,14 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseBadRequest, HttpResponseRedirect
 from ..forms.SignUpForm import SignUpForm
 from ..models import CustomUser, Statistic
+from .decorators import logout_required, request_type, RequestType
 
+@logout_required
+@request_type(RequestType.GET, RequestType.POST)
 def register(request: HttpRequest) -> HttpResponse:
-    if request.user.is_authenticated: return HttpResponseRedirect('/')
+    '''Controlleur de la page d'inscription
 
-    if request.method == 'GET':
-        return render(request, 'register.html')
+    Args:
+        request (HttpRequest): La requête HTTP
 
-    if request.method == 'POST':
+    Returns:
+        HttpResponse: La réponse HTTP à la requête d'inscription
+    '''
+
+    ret: HttpResponse = HttpResponseBadRequest()
+
+    if request.method == RequestType.GET.value:
+        ret = render(request, 'register.html')
+
+    elif request.method == RequestType.POST.value:
         form = SignUpForm(request.POST)
         if form.is_valid():
             statistic = Statistic.objects.create(
@@ -32,11 +44,11 @@ def register(request: HttpRequest) -> HttpResponse:
             with open('./static/icons/default-pfp.png', 'rb') as f:
                 user.profile_picture.save('default-pfp.png', f, save=True)
 
-            return HttpResponseRedirect('/login')
+            ret = HttpResponseRedirect('/login')
         
         else:
-            if form.errors.get('username'): return HttpResponseBadRequest('<p class="error">Ce nom d\'utilisateur est déjà pris.</p>')
-            if form.errors.get('email'): return HttpResponseBadRequest('<p class="error">Cette adresse email est déjà utilisée.</p>')
-            if form.errors.get('password2'): return HttpResponseBadRequest('<p class="error">Le mot de passe est trop commun.</p>')
+            if form.errors.get('username'): ret = HttpResponseBadRequest('<p class="error">Ce nom d\'utilisateur est déjà pris.</p>')
+            elif form.errors.get('email'): ret = HttpResponseBadRequest('<p class="error">Cette adresse email est déjà utilisée.</p>')
+            elif form.errors.get('password2'): ret = HttpResponseBadRequest('<p class="error">Le mot de passe est trop commun.</p>')
 
-    return HttpResponseBadRequest()
+    return ret
