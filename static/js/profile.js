142eaa34349a58dc8eb3c82527165d4ba895d2e1
Synel <70210528+Synell@users.noreply.github.com>
2023-11-14 08:55:15 +0100

    retour sur les méthodes JS qui fonctionnent parce que fetch ne fonctionne qu'avec GET

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,28 +1,24 @@
-async function readURL(input) {
+function readURL(input) {
     if (input.files && input.files[0]) {
         var csrf_token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
 
         var formData = new FormData();
         formData.append('pfp', input.files[0]);
+        var request = new XMLHttpRequest();
+        request.open('POST', '/change-pfp');
+        request.setRequestHeader('X-CSRFToken', csrf_token);
+        request.onload = function() {
+            document.querySelector(".notify").innerHTML = request.responseText;
 
-        await fetch('/change-pfp', {
-            method: 'POST',
-            headers: {
-                'X-CSRFToken': csrf_token
-            },
-            body: formData
-        })
-        .then(async function(response) {
-            document.querySelector(".notify").innerHTML = await response.text();
-
-            if (response.status == 200) {
+            if (request.status == 200) {
                 var reader = new FileReader();
                 reader.onload = function(e) {
                     document.querySelector('.profile-pic').setAttribute('src', e.target.result);
                 }
                 reader.readAsDataURL(input.files[0]);
             }
-        });
+        }
+        request.send(formData);
     }
 }
 
@@ -55,65 +51,56 @@ document.querySelectorAll(".sidebar-link").forEach(function(element) {
 
 
 
-document.querySelector("#change-pwd").addEventListener('submit', async function(event) {
+document.querySelector("#change-pwd").addEventListener('submit', function(event) {
     event.preventDefault();
     document.querySelector('form#change-pwd input[type="submit"]').disabled = true;
-
     var csrf_token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
-
     var formData = new FormData();
     formData.append('old-pwd', document.querySelector('input[name="old-pwd"]').value);
     formData.append('new-pwd', document.querySelector('input[name="new-pwd"]').value);
     formData.append('new-pwd-confirm', document.querySelector('input[name="new-pwd-confirm"]').value);
-
-    await fetch('/change-pwd', {
-        method: 'POST',
-        headers: {
-            'X-CSRFToken': csrf_token
-        },
-        body: formData
-    })
-    .then(async function(response) {
-        document.querySelector(".notify").innerHTML = await response.text();
-
-        if (response.status == 200) {
+    var request = new XMLHttpRequest();
+    request.open('POST', '/change-pwd');
+    request.setRequestHeader('X-CSRFToken', csrf_token);
+    request.onload = function() {
+        if (request.status == 400) {
+            document.querySelector(".notify").innerHTML = request.responseText;
+            document.querySelector('form#change-pwd input[type="submit"]').disabled = false;
+        } else if (request.status == 200) {
+            document.querySelector(".notify").innerHTML = request.responseText;
             setTimeout(function() {
                 window.location.reload();
             }, 1000);
-        } else {
-            document.querySelector('form#change-pwd input[type="submit"]').disabled = false;
         }
-    });
+    }
+    request.send(formData);
 });
 
 
 
 
-document.querySelector("#search-input").addEventListener('keyup', async function(event) {
+document.querySelector("#search-input").addEventListener('keyup', function(event) {
     var formData = new FormData();
     formData.append('username', event.target.value);
 
     var csrf_token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
 
-    await fetch('/search-notfriend-user', {
-        method: 'POST',
-        headers: {
-            'X-CSRFToken': csrf_token
-        },
-        body: formData
-    })
-    .then(async function(response) {
-        if (response.status == 200) {
-            document.querySelector("#user-datalist").innerHTML = await response.text();
+    var request = new XMLHttpRequest();
+    request.open('POST', '/search-notfriend-user');
+    request.setRequestHeader('X-CSRFToken', csrf_token);
+    request.onload = function() {
+        if (request.status == 200) {
+            document.querySelector("#user-datalist").innerHTML = request.responseText;
             htmx.process(document.querySelector("#user-datalist"));
         }
-    })
+    }
+    request.send(formData);
 });
 
 
 
-document.body.addEventListener("htmx:afterRequest", async function(event) {
-    setTimeout(async function() {
+document.body.addEventListener("htmx:afterRequest", function(event) {
+    setTimeout(function() {
         if (
             !(
                 (event.detail.xhr.responseURL.startsWith(window.location.origin + "/add-friend")) ||
@@ -124,52 +111,43 @@ document.body.addEventListener("htmx:afterRequest", async function(event) {
 
         document.querySelector("#search-input").dispatchEvent(new KeyboardEvent('keyup', {key: 'a'}));
 
+        var formData = new FormData();
         var csrf_token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
 
-        await fetch('/friend-list', {
-            method: 'GET',
-            headers: {
-                'X-CSRFToken': csrf_token
-            }
-        })
-        .then(async function(response) {
-            if (response.status == 200) {
-                document.querySelector("#friends-list").innerHTML = await response.text();
+        var request = new XMLHttpRequest();
+        request.open('GET', '/friend-list');
+        request.setRequestHeader('X-CSRFToken', csrf_token);
+        request.onload = function() {
+            if (request.status == 200) {
+                document.querySelector("#friends-list").innerHTML = request.responseText;
                 htmx.process(document.querySelector("#friends-list"));
             }
-        })
+        }
+        request.send(formData);
     }, 100);
 });
 
 
 
-document.querySelector("#delete-account").addEventListener('submit', async function(event) {
+document.querySelector("#delete-account").addEventListener('submit', function(event) {
     event.preventDefault();
     document.querySelector('form#delete-account input[type="submit"]').disabled = true;
-
     var csrf_token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
-
     var formData = new FormData();
     formData.append('password', document.querySelector('input[name="password"]').value);
-
-    await fetch('/delete-account', {
-        method: 'POST',
-        headers: {
-            'X-CSRFToken': csrf_token
-        },
-        redirect: 'follow',
-        body: formData
-    })
-    .then(async function(response) {
-        document.querySelector(".notify").innerHTML = await response.text();
-
-        if (response.redirected) {
+    var request = new XMLHttpRequest();
+    request.open('POST', '/delete-account');
+    request.setRequestHeader('X-CSRFToken', csrf_token);
+    request.onload = function() {
+        if (request.status == 400) {
+            document.querySelector(".notify").innerHTML = request.responseText;
+            document.querySelector('form#delete-account input[type="submit"]').disabled = false;
+        } else if (request.status == 200) {
+            document.querySelector(".notify").innerHTML = request.responseText;
             setTimeout(function() {
-                window.location.href = response.url;
+                window.location.href = "/login";
             }, 1000);
         }
-        else {
-            document.querySelector('form#delete-account input[type="submit"]').disabled = false;
-        }
-    });
+    }
+    request.send(formData);
 });
