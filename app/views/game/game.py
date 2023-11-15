979ca3549e9770d62e3c0c40f3ed2decac543c13
JohnGdr <john.gaudry@iut-dijon.u-bourgogne.fr>
2023-11-15 12:11:54 +0100

    [Back] Configuration lors de la création de la partie

--- app/views/game/game.py
+++ app/views/game/game.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, HttpResponseBadRequest
 from app.models import Game
-from ...logic import Board, Tile
+from ...logic import Board, Tile, RuleFactory
 import json
 from ..decorators import login_required, request_type, RequestType
 from ...http import HttpResponseNotifError
@@ -29,11 +29,11 @@ def game(request: HttpRequest) -> HttpResponse:
         except:
             return HttpResponseRedirect('/')
 
-        if game_inst.player1 != request.user and game_inst.player2 is None and not game_inst.is_private:
-            game_inst.player2 = request.user
-            game_inst.save()
+        if game_inst.game_participate.player1 != request.user and game_inst.game_participate.player2 is None and not game_inst.is_private:
+            game_inst.game_participate.player2 = request.user
+            game_inst.game_participate.save()
 
-        if game_inst.player1 != request.user and game_inst.player2 != request.user: return HttpResponseRedirect('/')
+        if game_inst.game_participate.player1 != request.user and game_inst.game_participate.player2 != request.user: return HttpResponseRedirect('/')
 
         ret = game_logic(request, game_inst)
 
@@ -63,11 +63,11 @@ def game_code(request: HttpRequest) -> HttpResponse:
         except:
             return HttpResponseNotifError('Code invalide.')
 
-        if game_inst.player1 != request.user and game_inst.player2 is None:
-            game_inst.player2 = request.user
-            game_inst.save()
+        if game_inst.game_participate.player1 != request.user and game_inst.game_participate.player2 is None:
+            game_inst.game_participate.player2 = request.user
+            game_inst.game_participate.save()
 
-        if game_inst.player1 != request.user and game_inst.player2 != request.user: return HttpResponseNotifError('Code invalide.')
+        if game_inst.game_participate.player1 != request.user and game_inst.game_participate.player2 != request.user: return HttpResponseNotifError('Code invalide.')
 
         ret = HttpResponseRedirect(f'/game?id={game_inst.id_game}')
 
@@ -86,13 +86,13 @@ def game_logic(request: HttpRequest, game: Game) -> HttpResponse:
     '''
 
     # TODO: LE CODE CI-DESSOUS EST TEMPORAIRE, IL FAUT LE MODIFIER DURANT LE SPRINT 3
-    board = Board(6)
+    board = Board(game.game_configuration.map_size, RuleFactory().get(game.game_configuration.counting_method))
     with open(game.move_list.path, 'r') as f:
         board.load(json.load(f))
 
     return render(
         request,
-        'game.html',
+        'game/game.html',
         {
             'board': board,
         }
