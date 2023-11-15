c1d92b268944566f4eb90a077effde782668676a
Synel <70210528+Synell@users.noreply.github.com>
2023-11-15 10:35:24 +0100

    correction de edit_tournament.py

--- app/views/tournament/edit_tournament.py
+++ app/views/tournament/edit_tournament.py
@@ -1,44 +1,59 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, HttpResponseBadRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseBadRequest
 from ...models.tournament import Tournament
 from datetime import datetime
-import random, string
+from ..decorators import login_required, request_type, RequestType
+from ...http import HttpResponseNotifError
 
+@login_required
+@request_type(RequestType.GET, RequestType.POST)
 def edit_tournament(request: HttpRequest, id_tournament: int) -> HttpResponse:
-    if not request.user.is_authenticated: return HttpResponseRedirect('/login')
+    '''Controller de la page de modification d'un tournoi
 
+    Args:
+        request (HttpRequest): Requête HTTP
+        id_tournament (int): Identifiant du tournoi
+
+    Returns:
+        HttpResponse: Réponse HTTP de redirection vers la page du tournoi modifié ou erreur
+    '''
+    ret: HttpResponse = HttpResponseNotifError('Erreur lors de la modification du tournois')
     try:
         tournament = Tournament.objects.get(id = id_tournament)
 
         if datetime.now().date() >= tournament.start_date:
-            return HttpResponseRedirect(f'/tournament?id={tournament.id}')
-
-    except:
-        return HttpResponseRedirect('/tournament')
-
-    if request.method == 'POST':
-        if (name := request.POST.get('tournament-name')) is None: return HttpResponseBadRequest('<p class="error">Le nom du tournoi est vide.</p>')
-        if (start_date := request.POST.get('start-date')) is None: return HttpResponseBadRequest('<p class="error">La date de début du tournoi est vide.</p>')
-        if (end_date := request.POST.get('end-date')) is None: return HttpResponseBadRequest('<p class="error">La date de fin du tournoi est vide.</p>')
-        if (organisator := request.POST.get('tournament-organizer')) is None: return HttpResponseBadRequest('<p class="error">L\'organisateur du tournoi est vide.</p>')
-        if (description := request.POST.get('tournament-desc', '')) is None: return HttpResponseBadRequest('<p class="error">La description du tournoi est vide.</p>')
-        if (player_min := request.POST.get('tournament-player-min')) is None: return HttpResponseBadRequest('<p class="error">Le nombre de joueurs minimum est vide.</p>')
-        private = bool(request.POST.get('tournament-private'))
-
-        try:
-            tournament.name = name
-            tournament.description = description
-            tournament.start_date = start_date
-            tournament.private = private
-            tournament.end_date = end_date
-            tournament.organisator = organisator
-            tournament.register_date = datetime.now().date()
-            tournament.player_min = player_min
-            tournament.save()
-
             return HttpResponse(f'/tournament?id={tournament.id}')
 
-        except:
-            return HttpResponseBadRequest('<p class="error">Erreur lors de la modification du tournois</p>')
+    except:
+        return HttpResponse('/tournament')
 
-    return render(request, 'tournament/edit_tournament.html', {'tournament': tournament, 'checked': 'checked' if tournament.private else ''})
+    if request.method == RequestType.POST.value:
+        if (name := request.POST.get('tournament-name')) is None: ret = HttpResponseNotifError('Le nom du tournoi est vide.')
+        elif (start_date := request.POST.get('start-date')) is None: ret = HttpResponseNotifError('La date de début du tournoi est vide.')
+        elif (end_date := request.POST.get('end-date')) is None: ret = HttpResponseNotifError('La date de fin du tournoi est vide.')
+        elif (organisator := request.POST.get('tournament-organizer')) is None: ret = HttpResponseNotifError('L\'organisateur du tournoi est vide.')
+        elif (description := request.POST.get('tournament-desc', '')) is None: ret = HttpResponseNotifError('La description du tournoi est vide.')
+        elif (player_min := request.POST.get('tournament-player-min')) is None: ret = HttpResponseNotifError('Le nombre de joueurs minimum est vide.')
+        else:
+            private = bool(request.POST.get('tournament-private'))
+
+            try:
+                tournament.name = name
+                tournament.description = description
+                tournament.start_date = start_date
+                tournament.private = private
+                tournament.end_date = end_date
+                tournament.organisator = organisator
+                tournament.register_date = datetime.now().date()
+                tournament.player_min = player_min
+                tournament.save()
+
+                ret = HttpResponse(f'/tournament?id={tournament.id}')
+
+            except:
+                ret = HttpResponseNotifError('Erreur lors de la modification du tournois.')
+
+    elif request.method == RequestType.GET.value:
+        ret = render(request, 'tournament/edit_tournament.html', {'tournament': tournament, 'checked': 'checked' if tournament.private else ''})
+
+    return ret
