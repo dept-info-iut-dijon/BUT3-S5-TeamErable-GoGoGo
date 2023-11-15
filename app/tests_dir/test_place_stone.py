d0f31198df59e469a21401d203a09e60baf695ec
Hugo-Mrula <hugo.mrula@iut-dijon.u-bourgogne.fr>
2023-11-15 09:41:11 +0100

    Ajout des initialisations de tests Sprint 3

--- app/tests_dir/test_place_stone.py
+++ app/tests_dir/test_place_stone.py
@@ -1,6 +1,15 @@
 from django.test import TestCase
 
 class PlaceStoneTestCase(TestCase):
+	def setUp(self):
+        self.client = Client()
+        self.sample_user = get_user_model().objects.create_user(
+            username='testuser',
+            email='testemail@sezamail.net',
+            password='testpassword123'
+        )
+		self.client.login(username='testuser', password='testpass')
+
 	def test_placement(self):
 		pass
 
