146b02c91091885b3542b8355d0da1ef0e3f279c
Hugo-Mrula <hugo.mrula@iut-dijon.u-bourgogne.fr>
2023-11-15 08:25:34 +0100

    Creation des classes de tests unitaires sprint 3
    Creation des fichier des tests unitaires avec les classes et methodes de tests vides

---
+++ app/tests_dir/test_init_game.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+class InitGameTestCase(TestCase):
+	def test_initialisation(self):
+		pass
+
+	def test_handicap(self):
+		pass
---
+++ app/tests_dir/test_surround_stone.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+
+class SurroundStoneTestCase(TestCase):
+	def test_surround(self):
+		pass
