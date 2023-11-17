bd9a4f0ff8eaff902ea49a78bc807912b8a9d4ac
Hugo-Mrula <hugo.mrula@iut-dijon.u-bourgogne.fr>
2023-11-17 15:50:08 +0100

    Mise a jour du test pour la creation de partie

--- app/tests_dir/games/test_init_game.py
+++ app/tests_dir/games/test_init_game.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 from ...logic import Board
 from ...models.custom_user import CustomUser
-from ...models.statistic import Statistic
+from ...models import Statistic, GameConfiguration, GameParticipate, Game
 
 class InitGameTestCase(TestCase):
     def setUp(self):
@@ -20,40 +20,43 @@ class InitGameTestCase(TestCase):
             password='testpassword123',
             stat=self.statistic
         )
-        self.client.login(username='testuser', password='testpass')
-        self.response = self.client.post('/create_game/', {
+        self.client.login(username='testuser', password='testpassword123')
+        self.response = self.client.post('/create-game', {
             'game-name': 'Test Game',
             'game-desc': 'Description for the test game',
             'game-private': False,
-            'map-size': '9x9',
-            'game-rules': 'japanese',
-            'byo_yomi':30,
+            'map-size': 9,
+            'counting-method': 'japanese',
+            'byo-yomi':30,
             'clock-type': 'Byo-yomi',
-            'time-clock':3600,
-            'komi':6.5
+            'time-clock':'01:00:00',
+            'komi':6.5,
+            'handicap': 2
         })
 
     # Test de la creation de partie
     def test_create_game(self):
         # On verifie si l'utilisateur est redirige vers la bonne page
-        self.assertEqual(self.response.status_code, 302)
-        self.assertTrue(self.response.url.startswith('/game?id='))
+        self.assertEqual(self.response.status_code, 200)
         # On verifie que l'objet Game est cree correctement
         created_game = Game.objects.get(name='Test Game')
+        self.assertEqual(created_game.name, 'Test Game')
         self.assertEqual(created_game.description, 'Description for the test game')
         self.assertEqual(created_game.done, False)
 
     # Test de l'initialisation correcte du terrain
     def test_initialisation(self):
         expected_board = [[None for _ in range(9)] for _ in range(9)]
-
-        actual_board = Board(9)
+        game = Game.objects.get(name='Test Game')
+        #actual_board = Board(9)
         with open(game.move_list.path, 'r') as f:
-            actual_board.load(json.load(f))
+            actual_board = Board(f)
+            #actual_board.load(json.load(f))
         # Verification de la taille du plateau
+        print(actual_board.size())
         self.assertEqual(Vector2(9, 9), actual_board.size())
         # Verification que le plateau est vide
-        self.assertEqual(expected_board, actual_board.raw())
+        #self.assertEqual(expected_board, actual_board.raw())
 
     def test_handicap(self):
         pass
