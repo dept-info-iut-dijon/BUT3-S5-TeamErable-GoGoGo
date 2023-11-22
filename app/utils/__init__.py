eba231fb6785815e03b5fc96946941ba0924cbef
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-11-22 10:33:02 +0100

    Création d'une fonction verify_tournament

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -5,5 +5,5 @@ from .classproperty import classproperty
 from .deprecated import deprecated
 from .Singleton import Singleton
 from .StaticClass import StaticClass
-from .verify import verify_post, verify_get
+from .verify import verify_post, verify_get, verify_tournament
 #----------------------------------------------------------------------
