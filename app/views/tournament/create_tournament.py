8bb23661c5bd642c1c35c66e37405c49dbcfc429
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-11-28 13:57:43 +0100

    Création des fonctions generation_code et can_create

--- app/views/tournament/create_tournament.py
+++ app/views/tournament/create_tournament.py
@@ -6,28 +6,43 @@ from datetime import datetime
 import random, string
 from ..decorators import login_required, request_type, RequestType
 from ...http import HttpResponseNotifError
-from ...utils import verify_post, verify_get
+from ...utils import verify_tournament
+from .tournament_struct import TournamentStruct
 
+def can_create_tournament(request: HttpRequest) -> bool :
+    '''Fonction permettant de savoir si il est possible de creer un tournoi
 
-def create_tournament_post(request: HttpRequest, name: str, description: str, start_date: datetime, end_date: datetime, organisator: str, player_min: int, map_size: str, counting_method: str, byo_yomi: int, clock_type: str, time_clock: str, komi: float, handicap: int) -> HttpResponse:
+    Args:
+        request (HttpRequest): Requête HTTP
+
+    Returns:
+        bool: True si il est possible de creer un tournoi, False sinon
+    '''
+    current_tournaments = Tournament.objects.filter(creator=request.user, end_date__gt = datetime.now().date()).all()
+
+    can_create = True
+    if current_tournaments.count() > 2:
+        can_create = False
+
+    return can_create
+
+def generation_code() -> str:
+    '''Fonction permettant de generer un code de 16 caractères
+
+    Returns:
+        str: Le code de 16 caractères
+    '''
+    code = None
+    while code is None or Tournament.objects.filter(code = code, end_date__gt = datetime.now()).exists():
+        code = ''.join(random.choice(string.ascii_uppercase + string.octdigits) for _ in range(16))
+    return code
+
+def create_tournament_post(request: HttpRequest, tournament_struct: TournamentStruct) -> HttpResponse:
     '''Créer un tournoi à partir des données du formulaire
 
     Args:
         request (HttpRequest): Requête HTTP
-        name (str): Nom du tournoi
-        description (str): Description du tournoi
-        start_date (datetime): Début du tournoi
-        end_date (datetime): Fin du tournoi
-        organisator (str): Organisateur du tournoi
-        player_min (int): Nombre de joueurs minimum
-
-        map_size (str): Taille de la carte pour la configuration des parties
-        counting_method (str): Methode de comptage pour la configuration des parties
-        byo_yomi (int): Byo-yomi pour la configuration des parties
-        clock_type (str): Type de horloge pour la configuration des parties
-        time_clock (int): Temps de jeu pour la configuration des parties
-        komi (float): Komi pour la configuration des parties
-        handicap (int): Handicap pour la configuration des parties
+        tournament_struct (TournamentStruct): Le tournoi a créer
 
     Returns:
         HttpResponse: Réponse HTTP de redirection vers la page du tournoi créé ou erreur
@@ -37,47 +52,37 @@ def create_tournament_post(request: HttpRequest, name: str, description: str, st
     ret: HttpResponse = HttpResponseBadRequest('Erreur lors de la création du tournois')
 
     try:
-        code = None
-        while code is None or Tournament.objects.filter(code = code, end_date__gt = datetime.now()).exists():
-            code = ''.join(random.choice(string.ascii_uppercase + string.octdigits) for _ in range(16))
-        
-        current_tournaments = Tournament.objects.filter(creator=request.user, end_date__gt = datetime.now().date()).all()
+        code = generation_code()
 
-        existing_tournament = False
-        if current_tournaments.count() > 0:
-            current_tournament = current_tournaments[0]
-            if datetime.now().date() >= current_tournament.start_date:
-                ret = HttpResponseNotifError('Un tournoi en cours existe déjà. Attendez la fin de celui-ci pour en créer un nouveau.')
-                existing_tournament = True
+        if can_create_tournament(request) is False:
+            ret = HttpResponseNotifError('Trop de tournois en cours. Attendez la fin de ceux-ci pour en creer un nouveau ou supprimez en un.')
 
-        if existing_tournament is False:
-            current_tournaments.delete()
-
-            list_str = time_clock.split(':')
-            time_clock = int(list_str[0]) * 3600 + int(list_str[1]) * 60 + int(list_str[2])
+        else:
+            list_str = tournament_struct.time_clock.split(':')
+            tournament_struct.time_clock = int(list_str[0]) * 3600 + int(list_str[1]) * 60 + int(list_str[2])
 
             game_configuration = GameConfiguration.objects.create(
-                map_size = map_size,
-                counting_method = counting_method,
-                byo_yomi = byo_yomi,
-                clock_type = clock_type,
-                clock_value = time_clock,
-                komi = komi,
-                handicap = handicap,
+                map_size = tournament_struct.map_size,
+                counting_method = tournament_struct.counting_method,
+                byo_yomi = tournament_struct.byo_yomi,
+                clock_type = tournament_struct.clock_type,
+                clock_value = tournament_struct.time_clock,
+                komi = tournament_struct.komi,
+                handicap = tournament_struct.handicap,
                 is_private = False
             )
 
             tournament = Tournament.objects.create(
-                name = name,
-                description = description,
-                start_date = start_date,
+                name = tournament_struct.name,
+                description = tournament_struct.description,
+                start_date = tournament_struct.start_date,
                 private = private,
-                end_date = end_date,
-                organisator = organisator,
+                end_date = tournament_struct.end_date,
+                organisator = tournament_struct.organisator,
                 creator = request.user,
                 register_date = datetime.now().date(),
                 code = code,
-                player_min = player_min,
+                player_min = tournament_struct.player_min,
                 game_configuration = game_configuration
             )
 
@@ -103,25 +108,10 @@ def create_tournament(request: HttpRequest) -> HttpResponse:
     ret: HttpResponse = HttpResponseBadRequest('Erreur lors de la création du tournois')
     if request.method == RequestType.POST.value:
         
-        try:
-            name = verify_post(request, 'tournament-name', 'Le nom du tournoi est vide.')
-            start_date = verify_post(request, 'start-date', 'La date de début du tournoi est vide.')
-            end_date = verify_post(request, 'end-date', 'La date de fin du tournoi est vide.')
-            organisator = verify_post(request, 'tournament-organizer', 'L\'organisateur du tournoi est vide.')
-            description = verify_post(request, 'tournament-desc', 'La description du tournoi est vide.')
-            player_min = verify_post(request, 'tournament-player-min', 'Le nombre de joueurs minimum est vide.')
-            map_size = verify_post(request, 'tournament-map-size', 'La taille de la carte est vide.')
-            counting_method = verify_post(request, 'tournament-counting-method', 'La methode de comptage est vide.')
-            byo_yomi = verify_post(request, 'tournament-byo_yomi', 'Le temps de jeu est vide.')
-            clock_type = verify_post(request, 'tournament-clock-type', 'Le type de horloge est vide.')
-            time_clock = verify_post(request, 'tournament-time-clock', 'Le temps de jeu est vide.')
-            komi = verify_post(request, 'tournament-komi', 'Le Komi est vide.')
-            handicap = verify_post(request, 'tournament-handicap', 'Le Handicap est vide.')
+        if (tournament_verif := verify_tournament(request)) is Exception:
+            return HttpResponseNotifError(tournament_verif)
 
-        except Exception as e:
-            return HttpResponseNotifError(str(e))
-
-        ret = create_tournament_post(request, name, description, start_date, end_date, organisator, player_min, map_size, counting_method, byo_yomi, clock_type, time_clock, komi, handicap)
+        ret = create_tournament_post(request, tournament_verif)
 
     elif request.method == RequestType.GET.value:
         ret = render(request, 'tournament/create_tournament.html')
