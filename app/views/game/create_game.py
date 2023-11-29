4d177668296c8463fcf9893764ad53dd124cb7fd
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-11-29 15:52:20 +0100

    Séparation et création de constructeurs

--- app/views/game/create_game.py
+++ app/views/game/create_game.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseBadRequest
 from ...models.game import Game
+from ...models.tournament import Tournament
 from ...models.game_participate import GameParticipate
+from ...models.game_configuration import GameConfiguration
 from datetime import datetime
 import random, string, os, json
 from ...logic import Board, RuleFactory
@@ -11,47 +13,28 @@ from ..code_manager import CodeManager
 from ...http import verify_game
 from .game_struct import GameStruct
 from .game_configuration import create_game_config
-from ...storage import GameStorage
 
-def _create_new_game(request : HttpRequest, game_struct: GameStruct) -> HttpRequest:
+def _create_new_game(request : HttpRequest, game_struct: GameStruct, id_tournament: int) -> HttpRequest:
     '''Fonction permettant de creer une nouvelle partie
     
     Args:
         request (HttpRequest): Requête HTTP
         game_srtuct (GameStruct): La nouvelle partie
+        id_tournament (int): L'id du tournoi
 
     Returns:
         HttpRequest: La requête HTTP
     '''
-    code = CodeManager().generate_game_code()
+    partcipate = construct_participate(request.user.id)
+    game = construct_game(game_struct, partcipate, id_tournament)
 
-    configuration = create_game_config(game_struct.game_configuration)
-
-    participate = GameParticipate.objects.create(
-        player1 = request.user,
-        player2 = None,
-        score_player1 = 0,
-        score_player2 = 0
-    )
-    
-    game = Game.objects.create(
-        name = game_struct.name,
-        description = game_struct.description,
-        code = code,
-        start_date = datetime.now(),
-        duration = 0,
-        done = False,
-        tournament = None,
-        game_configuration = configuration,
-        game_participate = participate
-    )
-    
     file = f'dynamic/games/{game.id_game:X}.json'
 
     if not os.path.exists('dynamic/games'):
         os.makedirs('dynamic/games')
-    b = Board(int(game_struct.game_configuration.map_size), float(game_struct.game_configuration.komi), RuleFactory().get(game_struct.game_configuration.counting_method))
-    GameStorage.save_game(file, b)
+    with open(file, 'w') as f:
+        b = Board(int(game_struct.game_configuration.map_size), float(game_struct.game_configuration.komi), RuleFactory().get(game_struct.game_configuration.counting_method))
+        json.dump(b.export(), f)
 
     game.move_list = file
     game.save()
@@ -60,14 +43,47 @@ def _create_new_game(request : HttpRequest, game_struct: GameStruct) -> HttpRequ
 
     return ret
 
+def construct_participate(id_player1: int, id_player2: int = None) -> GameParticipate:
+    participate = GameParticipate.objects.create(
+        player1 = id_player1,
+        player2 = id_player2,
+        score_player1 = 0,
+        score_player2 = 0
+    )
+    return participate
+
+def construct_game(game_struct: GameStruct, participate: GameParticipate, id_tournament: int = None) -> Game:
+        
+    code = CodeManager().generate_game_code()
+
+    if id_tournament is None:
+        configuration = create_game_config(game_struct.game_configuration)
+    else:
+        configuration = Tournament.objects.get(id_tournament=id_tournament).game_configuration
+
+    game = Game.objects.create(
+        name = game_struct.name,
+        description = game_struct.description,
+        code = code,
+        start_date = datetime.now(),
+        duration = 0,
+        done = False,
+        tournament = id_tournament,
+        game_configuration = configuration,
+        game_participate = participate
+    )
+
+    return game
+
 
 @login_required
 @request_type(RequestType.GET, RequestType.POST)
-def create_game(request: HttpRequest) -> HttpResponse:
+def create_game(request: HttpRequest, id_tournament: int = None) -> HttpResponse:
     '''Controlleur de la page de création de partie
 
     Args:
         request (HttpRequest): La requête HTTP
+        id_tournament (int): L'id du tournoi ou None
 
     Returns:
         HttpResponse: La réponse HTTP à la requête de création de partie
@@ -83,8 +99,7 @@ def create_game(request: HttpRequest) -> HttpResponse:
             ret = HttpResponseNotifError('Trop de parties en cours. Attendez la fin de ceux-ci pour en creer une nouvelle.')
         
         else:
-            ret = _create_new_game(request, game_verif)
-                
+            ret = _create_new_game(request, game_verif, id_tournament)
 
     elif request.method == RequestType.GET.value:
         ret = render(request, 'game/create_game.html')
