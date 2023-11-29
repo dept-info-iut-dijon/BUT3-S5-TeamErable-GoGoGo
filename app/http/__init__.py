055ba4a106c778b0c0ce16043e4a74159aadfdd1
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-11-29 08:41:09 +0100

    Création de fonctions pour vérifié game et game_config

--- app/http/__init__.py
+++ app/http/__init__.py
@@ -1,3 +1,3 @@
 from .HttpResponseNotifError import HttpResponseNotifError
 from .HttpResponseNotifSuccess import HttpResponseNotifSuccess
-from .verify import verify_post, verify_get, verify_tournament
+from .verify import verify_post, verify_get, verify_tournament, verify_game
