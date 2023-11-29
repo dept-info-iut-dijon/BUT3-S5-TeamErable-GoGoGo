3bac8263bc317daa055b16f3ac3fa411e46410a9
HugoMrula <hugo.mrula@iut-dijon.u-bourgogne.fr>
2023-11-29 15:53:09 +0100

    Ajout intialisation de timer

--- app/logic/timer/Timer.py
+++ app/logic/timer/Timer.py
@@ -2,23 +2,23 @@ from abc import ABC, abstractmethod
 from datetime import datetime, timedelta
 
 class Timer(ABC):
+
+    @abstractmethod
     def __init__(self):
         pass
-    """"
-    @abstractmethod
+    
     def start(self):
-        '''
-            Initialise et lance le timer
-        '''
-        pass
+        '''Lance le timer'''
+        self.started = True
+
+    def isStarted(self):
+        return self.started
 
     @abstractmethod
     def make_move(self):
-        '''
-            Deduit le temps mis pour faire le coup du timer
-        '''
+        '''Retire le temps mis pour faire le coup du timer'''
         pass
-
+    """
     @abstractmethod
     def get_remaining_time(self)->int:
         '''Permet d'obtenir le temps restant au joueur
