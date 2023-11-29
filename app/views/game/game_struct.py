a939c810b43422d5bfed73ba827cdef28b0cde60
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-11-29 17:39:13 +0100

    Correction bug test à cause de dépendances

--- app/views/game/game_struct.py
+++ app/views/game/game_struct.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from ...http import verify_post
+from django.http import HttpRequest
+from typing import Union
 from .game_configuration_struct import GameConfigurationStruct
 
 @dataclass
@@ -6,3 +9,24 @@ class GameStruct:
     name : str
     description : str
     game_configuration : GameConfigurationStruct
+
+    @classmethod
+    def verify_game(cls, request: HttpRequest) -> Union["GameStruct", Exception]:
+        ret = None
+        try: 
+            game_config = GameConfigurationStruct.verify_game_config(request)
+
+            ret = cls(
+                name = verify_post(request, 'game-name', 'Le nom de la partie est vide.'),
+                description = verify_post(request, 'game-desc', 'La description de la partie est vide.'),
+                game_configuration = game_config
+            )
+
+        except Exception as e:
+            ret = e
+        
+        return ret
+    
+    def __str__(self):
+        return (f"GameStruct(name={self.name}, "
+                f"description={self.description}) ")
