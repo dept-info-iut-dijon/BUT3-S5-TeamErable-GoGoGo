0c4c55b00de46f5e9d40df11317fc3eaad3259a7
HugoMrula <hugo.mrula@iut-dijon.u-bourgogne.fr>
2023-11-30 10:45:18 +0100

    Implementation du Byoyomi Japonais

--- app/logic/timer/Timer.py
+++ app/logic/timer/Timer.py
@@ -1,32 +1,55 @@
 from abc import ABC, abstractmethod
 from datetime import datetime, timedelta
+from ..Tile import Tile
 
 class Timer(ABC):
 
     @abstractmethod
-    def __init__(self):
+    def __init__(self, timer_data: dict):
+        '''
+        Methode du constructeur pour le minuteur
+
+        Args :
+            timer_data (dict): Dictionnaire contenant les donnees de configuration du minuteur.
+        '''
         pass
     
-    def start(self):
-        '''Lance le timer'''
-        self.started = True
-
-    def isStarted(self):
-        return self.started
 
     @abstractmethod
-    def make_move(self):
-        '''Retire le temps mis pour faire le coup du timer'''
+    def set_move(self):
+        '''Sauvegarde le temps du nouveau placement'''
         pass
-    """
+
+
     @abstractmethod
-    def get_remaining_time(self)->int:
-        '''Permet d'obtenir le temps restant au joueur
-        Returns:
-            int: Nombre de secondes correspondant au temps de jeu
+    def make_move(self, player: Tile):
+        '''
+        Gere le calcul du temps pour chaque coup.
+
+        Args :
+            player (Tile): Le joueur effectuant le coup (Noir ou Blanc).
+
+        Returns :
+            bool: True si la partie est terminee, False sinon.
         '''
         pass
 
     @abstractmethod
-    def is_timeout(self):
-        pass"""
+    def get_initial_time(self)->float:
+        '''
+            Permet d'obtenir le temps depuis le dernier coup
+
+            Returns:
+                float: le temps depuis le dernier coup
+        '''
+        pass
+
+    @abstractmethod
+    def get_separate_timers(self)->(int, int):
+        '''
+            Permet d'obtenir l'etat des timers separes
+
+            Returns:
+                (int, int): Tuple contenant l'etat des timers blanc et noir
+        '''
+        pass
