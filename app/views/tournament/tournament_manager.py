73a1e6dcbfe9f4d7863a0380b80c4db89f6566a3
Samuel MONTAGNA (Synel dans les anciens commits) <70210528+Synell@users.noreply.github.com>
2023-12-14 14:22:34 +0100

    nettoyage du code

--- app/views/tournament/tournament_manager.py
+++ app/views/tournament/tournament_manager.py
@@ -9,31 +9,6 @@ from ...http import HttpResponseNotifError
 from ...tournament_logic import Tournament as TournamentLogic, IMatch, Bracket, Match, FakeMatch, Player
 from random import shuffle
 
-# def _has_game_happened(tournament_games:list[Game], player1:CustomUser, player2:CustomUser)->GameParticipate:
-#     '''
-#         Verifie si la partie a eut lieu
-#         Args:
-#             tournament_games (List[Game]): liste des parties liees au tournois
-#             player1 (CustomUser): le joueur 1 de la partie concernee
-#             player2 (CustomUser): le joueur 2 de la partie concernee
-
-#         Returns:
-#             GameParticipate: les lien de participation ou None si elle n'existe pas
-#     '''
-#     ret = None
-
-#     for game in tournament_games:
-#         game_participate = GameParticipate.objects.filter(
-#             Q(id_game_participate=game.game_participate_id) &
-#             (Q(player1=player1) | Q(player1=player2)) &
-#             (Q(player2=player1) | Q(player2=player2))
-#         )
-#         if game_participate.count() == 1:
-#             ret = game_participate
-#             break
-
-#     return ret
-
 
 def generate_match(request: HttpRequest, match: Match) -> str:
     if match is None:
@@ -206,97 +181,6 @@ def _get_tournament_participants(tournament:Tournament)->list[CustomUser]:
     participate = ParticipateTournament.objects.filter(tournament=tournament)
     participants = [entry.person for entry in participate]
     return participants
-
-# def _generate_tournament_tree(participants:list[CustomUser], games:list[Game])->list[list]:
-#     '''
-#         Genere l'arbre de tournois
-#         Args:
-#             participants (list[CustomUser]): la liste des participants
-#             games (list[Game]): la liste des parties liees au tournois
-
-#         Returns:
-#             list[list]: la liste des rounds
-#     '''
-#     tournament_games = []
-#     current_round = [(participants[i - 1], participants[i]) for i in range(1, len(participants), 2)]
-#     lone_member = _get_lone_member(participants)
-#     round_iter = 0
-
-#     while len(current_round) != 0:
-#         tournament_games.append([])
-#         next_round = []
-#         if lone_member != None:
-#             next_round.append(lone_member)
-
-#         for match in current_round:
-#             curr_game_participate = _has_game_happened(games, match[0], match[1])
-#             winner = _get_winner(match[0], match[1], curr_game_participate)
-#             if winner != None : 
-#                 next_round.append(match[winner])
-#             else:
-#                 next_round.append(None)
-#             tournament_games[round_iter].append((match[0], match[1], winner))
-
-#         lone_member = _get_lone_member(next_round)
-#         current_round = _create_next_round(next_round)
-#         round_iter += 1
-#     return tournament_games
-
-# def _get_lone_member(participants:list[CustomUser])->CustomUser:
-#     '''
-#         Permet d'obtenir un eventuel participant impaire
-
-#         Args:
-#             participants (CustomUser): liste des participants 
-
-#         Returns:
-#             CustomUser: Le participant impaire
-#     '''
-#     lone_member = None
-#     if len(participants) % 2 == 1:
-#         lone_member = participants[-1]
-#     return lone_member
-
-# def _create_next_round(next_round:list[CustomUser])->list:
-#     '''
-#         Cree la liste des mathcs du prochain round
-
-#         Args:
-#             next_round (list[CustomUser]): liste de gagnants du round
-
-#         Returns:
-#             list: liste des matchs du prochain round
-
-#     '''
-
-#     current_round = [(next_round[i - 1], next_round[i]) for i in range(1, len(next_round), 2)]
-        
-#     return current_round
-
-# def _get_winner(player1:CustomUser, player2:CustomUser, curr_game_participate:GameParticipate)-> int:
-#     '''
-#         Permet d'obtenir le gagnant d'une partie
-
-#         Args:
-#             player1 (CustomUser): le joueur 1
-#             player2 (CustomUser): le joueur 2
-#             curr_game (Game): le lien de participation a la partie 
-        
-#         Returns:
-#             int: position du gagnant dans l'association (0 ou 1)
-#     '''
-#     winner=None
-
-#     if curr_game_participate != None and curr_game_participate[0]:
-#         score_player1 = curr_game_participate[0].score_player1
-#         score_player2 = curr_game_participate[0].score_player2
-
-#         if score_player1 > score_player2:
-#             winner = 0
-#         elif score_player2 > score_player1:
-#             winner = 1
-
-#     return winner
 
 
 @login_required
