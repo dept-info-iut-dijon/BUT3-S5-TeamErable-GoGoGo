88d9fbcdb5d1b2d39380cb2f71f27f0eaaa10058
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-12-14 11:27:15 +0100

    Page carrière (front + une partie du back)

--- app/urls.py
+++ app/urls.py
@@ -50,6 +50,10 @@ urlpatterns = [
     path("game-code", views.game_code, name="game_code"),
     path("game-view-player", views.game_view_player, name="game_view_player"),
 
+    #Carrière
+    path("search-games-historic", views.search_games_historic, name="search_games_historic"),
+    path("career", views.career, name="career"),
+
     #Classement
     path("classement-global", views.classement_global, name="classement_global"),
 
