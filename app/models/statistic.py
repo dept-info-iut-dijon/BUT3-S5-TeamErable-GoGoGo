1b7a12f9490f60bd313132415a2d165de9c562f5
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-12-14 14:11:25 +0100

    Modification table statistic

--- app/models/statistic.py
+++ app/models/statistic.py
@@ -2,10 +2,14 @@ from django.db import models
 
 class Statistic(models.Model):
     '''Classe permettant de creer une table qui répertorie les statistiques des joueurs'''
-    game_win = models.IntegerField()
-    game_loose = models.IntegerField()
-    game_ranked_win = models.IntegerField()
-    game_ranked_loose = models.IntegerField()
+    game_win = models.IntegerField(default=0)
+    game_loose = models.IntegerField(default=0)
+    game_ranked_win = models.IntegerField(default=0)
+    game_ranked_loose = models.IntegerField(default=0)
+    tournament_win = models.IntegerField(default=0)
+    tournament_loose = models.IntegerField(default=0)
+    elo = models.IntegerField(default=1000)
+    rang = models.CharField(max_length=100, default="30ème Kyu")
 
 
 
