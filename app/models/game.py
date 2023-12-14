9f94b8716c5a46e461304b42e27354e6735d9a1f
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-12-14 13:59:27 +0100

    Ajout du champ save pour game

--- app/models/game.py
+++ app/models/game.py
@@ -12,6 +12,7 @@ class Game(models.Model):
     start_date = models.DateField()
     duration = models.IntegerField()
     done = models.BooleanField(default=False)
+    save = models.BooleanField(default=False)
     move_list = models.FileField(upload_to='dynamic/games', null=True)
     tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, related_name='tournament', null=True)
     game_configuration = models.ForeignKey(GameConfiguration, on_delete=models.CASCADE, null=False)
