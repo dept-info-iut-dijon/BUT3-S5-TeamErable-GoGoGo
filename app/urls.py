80a408c2c5ff608076b2b8aefe08e1c230cf1854
MatthieuThivard <matthieu.thivard@iut-dijon.u-bourgogne.fr>
2023-12-15 11:16:40 +0100

    Fonctionnalité import (front + back) Il faut modifié le CSS

--- app/urls.py
+++ app/urls.py
@@ -53,6 +53,7 @@ urlpatterns = [
     #Carrière
     path("search-games-historic", views.search_games_historic, name="search_games_historic"),
     path("career", views.career, name="career"),
+    path("import-JSON", views.import_JSON, name="import_JSON"),
 
     #Classement
     path("classement-global", views.global_ranking, name="classement_global"),
